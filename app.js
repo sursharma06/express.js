ee58c6cacc122dbc14e8b0c6124d8f02e7721f67
Surbhi Sharma <sharma.surbhi13@gmail.com>
2017-10-04 13:53:25 -0400

    req.params

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('this is my first express app!!');
+app.get('/user/:name', function (req, res) {
+  res.send(req.params.name);
 });
+// app.get('/', function (req, res) {
+//   res.send('this is my first express app!!');
+// });
+//
+// app.get('/pets/hamster', function (req, res) {
+//   res.send('this is another response!');
+// });
+//
+// app.get('/pets', function (req, res) {
+//   res.send('this is the third response to a new GET request');
+// });
 
 app.listen(3000, function () {
   console.log('listening on port 3000');
